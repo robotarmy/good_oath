e7331f02e2b016dbe0fd5c3254ffb17b6b72d6ad
Curtis Schofield <github.com@robotarmyma.de>
2010-08-05 17:59:47 -0700

    nooooopo?

---
+++ spec/api_spec.rb
@@ -0,0 +1,22 @@
+require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
+describe 'API' do
+  # authorize once
+  before(:all) do
+    @goat = GoodOath.new
+    agent = Agent.new(@goat).authorize_user('bob.fake.4@robotarmyma.de','oauth')
+  end
+  it "can load a user id" do
+    @goat = GoodOath.new
+    @goat.load_user('bob.fake.4@robotarmyma.de')
+    api = Api.new(@goat)
+    api.user_id.should == '4113025'
+  end
+  it "can get a list of books for my shelf when i have a private profile" do
+    # bob has a private profile
+    @goat = GoodOath.new
+    @goat.load_user('bob.fake.4@robotarmyma.de')
+    api = Api.new(@goat)
+    api.search_reviews('dune').size.should > 0
+  end
+
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,9 @@
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 require 'good_oath'
+require 'api'
+require 'agent'
+require 'goat'
 require 'spec'
 require 'spec/autorun'
 
